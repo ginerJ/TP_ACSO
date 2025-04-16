6e0ffbb5019a0e669afad791f3ce516e06d45e0b
CodderG <127430228+ginerJ@users.noreply.github.com>
2025-04-16 13:52:00 -0300

    p1

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -1,4 +1,3 @@
-; /** defines bool y puntero **/
 %define NULL 0
 %define TRUE 1
 %define FALSE 0
@@ -12,17 +11,168 @@ global string_proc_node_create_asm
 global string_proc_list_add_node_asm
 global string_proc_list_concat_asm
 
-; FUNCIONES auxiliares que pueden llegar a necesitar:
 extern malloc
 extern free
 extern str_concat
-
+extern strlen
+extern strcpy
+extern strcat
 
 string_proc_list_create_asm:
-
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 16
+        mov     edi, 16
+        call    malloc
+        mov     qword [rbp-8], rax
+        cmp     qword [rbp-8], 0
+        jne     .L2
+        mov     eax, 0
+        jmp     .L3
+.L2:
+        mov     rax, qword [rbp-8]
+        mov     qword [rax], 0
+        mov     rax, qword [rbp-8]
+        mov     qword [rax+8], 0
+        mov     rax, qword [rbp-8]
+.L3:
+        leave
+        ret
 string_proc_node_create_asm:
-
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 32
+        mov     eax, edi
+        mov     qword [rbp-32], rsi
+        mov     byte [rbp-20], al
+        mov     edi, 32
+        call    malloc
+        mov     qword [rbp-8], rax
+        cmp     qword [rbp-8], 0
+        jne     .L5
+        mov     eax, 0
+        jmp     .L6
+.L5:
+        mov     rax, qword [rbp-8]
+        mov     qword [rax], 0
+        mov     rax, qword [rbp-8]
+        mov     qword [rax+8], 0
+        mov     rax, qword [rbp-8]
+        movzx   edx, byte [rbp-20]
+        mov     byte [rax+16], dl
+        mov     rax, qword [rbp-8]
+        mov     rdx, qword [rbp-32]
+        mov     qword [rax+24], rdx
+        mov     rax, qword [rbp-8]
+.L6:
+        leave
+        ret
 string_proc_list_add_node_asm:
-
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 48
+        mov     qword [rbp-24], rdi
+        mov     eax, esi
+        mov     qword [rbp-40], rdx
+        mov     byte [rbp-28], al
+        movzx   eax, byte [rbp-28]
+        mov     rdx, qword [rbp-40]
+        mov     rsi, rdx
+        mov     edi, eax
+        call    string_proc_node_create_asm
+        mov     qword [rbp-8], rax
+        cmp     qword [rbp-8], 0
+        je      .L12
+        mov     rax, qword [rbp-24]
+        mov     rax, qword [rax]
+        test    rax, rax
+        jne     .L10
+        mov     rax, qword [rbp-24]
+        mov     rdx, qword [rbp-8]
+        mov     qword [rax], rdx
+        jmp     .L11
+.L10:
+        mov     rax, qword [rbp-24]
+        mov     rax, qword [rax+8]
+        mov     rdx, qword [rbp-8]
+        mov     qword [rax], rdx
+        mov     rax, qword [rbp-24]
+        mov     rdx, qword [rax+8]
+        mov     rax, qword [rbp-8]
+        mov     qword [rax+8], rdx
+.L11:
+        mov     rax, qword [rbp-24]
+        mov     rdx, qword [rbp-8]
+        mov     qword [rax+8], rdx
+        jmp     .L7
+.L12:
+        nop
+.L7:
+        leave
+        ret
 string_proc_list_concat_asm:
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 64
+        mov     qword [rbp-40], rdi
+        mov     eax, esi
+        mov     qword [rbp-56], rdx
+        mov     byte [rbp-44], al
+        cmp     qword [rbp-40], 0
+        je      .L14
+        cmp     qword [rbp-56], 0
+        jne     .L15
+.L14:
+        mov     eax, 0
+        jmp     .L16
+.L15:
+        mov     rax, qword [rbp-56]
+        mov     rdi, rax
+        call    strlen
+        add     rax, 1
+        mov     rdi, rax
+        call    malloc
+        mov     qword [rbp-8], rax
+        cmp     qword [rbp-8], 0
+        jne     .L17
+        mov     eax, 0
+        jmp     .L16
+.L17:
+        mov     rdx, qword [rbp-56]
+        mov     rax, qword [rbp-8]
+        mov     rsi, rdx
+        mov     rdi, rax
+        call    strcpy
+        mov     rax, qword [rbp-40]
+        mov     rax, qword [rax]
+        mov     qword [rbp-16], rax
+        jmp     .L18
+.L20:
+        mov     rax, qword [rbp-16]
+        movzx   eax, byte [rax+16]
+        cmp     byte [rbp-44], al
+        jne     .L19
+        mov     rax, qword [rbp-16]
+        mov     rdx, qword [rax+24]
+        mov     rax, qword [rbp-8]
+        mov     rsi, rdx
+        mov     rdi, qword [rbp-8]
+        call    str_concat
+        mov     qword [rbp-24], rax
+        mov     rax, qword [rbp-8]
+        mov     rdi, rax
+        call    free
+        mov     rax, qword [rbp-24]
+        mov     qword [rbp-8], rax
+.L19:
+        mov     rax, qword [rbp-16]
+        mov     rax, qword [rax]
+        mov     qword [rbp-16], rax
+.L18:
+        cmp     qword [rbp-16], 0
+        jne     .L20
+        mov     rax, qword [rbp-8]
+.L16:
+        leave
+        ret
 
--- TP2-x86_64/ej1/ej1.c
+++ TP2-x86_64/ej1/ej1.c
@@ -1,66 +1,118 @@
 #include "ej1.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
 
 string_proc_list* string_proc_list_create(void){
+    string_proc_list* list = (string_proc_list*)malloc(sizeof(string_proc_list));
+    if(!list){
+        fprintf(stderr, "Error: Fallo alocacion de memoria para la lista\n");
+        return NULL;
+    }
+    list->first = NULL;
+    list->last  = NULL;
+    return list;
 }
 
 string_proc_node* string_proc_node_create(uint8_t type, char* hash){
+    string_proc_node* node = (string_proc_node*)malloc(sizeof(string_proc_node));
+    if(!node){
+        fprintf(stderr, "Error: Fallo alocacion de memoria para el nodo\n");
+        return NULL;
+    }
+    node->next = NULL;
+    node->previous = NULL;
+    node->type = type;
+    node->hash = hash;
+    return node;
 }
 
 void string_proc_list_add_node(string_proc_list* list, uint8_t type, char* hash){
+    string_proc_node* node = string_proc_node_create(type, hash);
+    if(!node)
+        return;
+
+    if(!list->first)
+        list->first = node;
+    else {
+        list->last->next = node;
+        node->previous = list->last;
+    }
+
+    list->last = node;
 }
 
-char* string_proc_list_concat(string_proc_list* list, uint8_t type , char* hash){
+char* string_proc_list_concat(string_proc_list* list, uint8_t type, char* hash) {
+    if (!list || !hash)
+        return NULL;
+
+    // Reemplazo de strdup
+    char* dup = (char*)malloc(strlen(hash) + 1);
+    if (!dup) {
+        fprintf(stderr, "Error: No se logro duplicar el hash\n");
+        return NULL;
+    }
+    strcpy(dup, hash);
+
+    for (string_proc_node* current = list->first; current; current = current->next) {
+        if (current->type == type) {
+            char* temp = str_concat(dup, current->hash);
+            free(dup);
+            dup = temp;
+        }
+    }
+    return dup;
 }
+
 
 
 /** AUX FUNCTIONS **/
 
 void string_proc_list_destroy(string_proc_list* list){
 
-	/* borro los nodos: */
-	string_proc_node* current_node	= list->first;
-	string_proc_node* next_node		= NULL;
-	while(current_node != NULL){
-		next_node = current_node->next;
-		string_proc_node_destroy(current_node);
-		current_node	= next_node;
-	}
-	/*borro la lista:*/
-	list->first = NULL;
-	list->last  = NULL;
-	free(list);
+    /* borro los nodos: */
+    string_proc_node* current_node	= list->first;
+    string_proc_node* next_node		= NULL;
+    while(current_node != NULL){
+        next_node = current_node->next;
+        string_proc_node_destroy(current_node);
+        current_node	= next_node;
+    }
+    /*borro la lista:*/
+    list->first = NULL;
+    list->last  = NULL;
+    free(list);
 }
 void string_proc_node_destroy(string_proc_node* node){
-	node->next      = NULL;
-	node->previous	= NULL;
-	node->hash		= NULL;
-	node->type      = 0;			
-	free(node);
+    node->next      = NULL;
+    node->previous	= NULL;
+    node->hash		= NULL;
+    node->type      = 0;
+    free(node);
 }
 
-
 char* str_concat(char* a, char* b) {
-	int len1 = strlen(a);
-    int len2 = strlen(b);
-	int totalLength = len1 + len2;
-    char *result = (char *)malloc(totalLength + 1); 
+    int totalLength = strlen(a) + strlen(b);
+    char *result = (char *)malloc(totalLength + 1);
     strcpy(result, a);
     strcat(result, b);
-    return result;  
+    return result;
 }
 
 void string_proc_list_print(string_proc_list* list, FILE* file){
-        uint32_t length = 0;
-        string_proc_node* current_node  = list->first;
-        while(current_node != NULL){
-                length++;
-                current_node = current_node->next;
-        }
-        fprintf( file, "List length: %d\n", length );
-		current_node    = list->first;
-        while(current_node != NULL){
-                fprintf(file, "\tnode hash: %s | type: %d\n", current_node->hash, current_node->type);
-                current_node = current_node->next;
-        }
+    uint32_t length = 0;
+    string_proc_node* current_node  = list->first;
+    while(current_node != NULL){
+        length++;
+        current_node = current_node->next;
+    }
+    fprintf( file, "List length: %d\n", length );
+    current_node    = list->first;
+    while(current_node != NULL){
+        fprintf(file, "\tnode hash: %s | type: %d\n", current_node->hash, current_node->type);
+        current_node = current_node->next;
+    }
 }
-
